2b612ae1e9a2d9daa0174c13cab7619b69404f93
Mark <googlcraftlol@gmail.com>
2021-07-23 02:38:05 +0300

    removed models for a while

--- src/main/java/ru/mar4elkin/Main.java
+++ src/main/java/ru/mar4elkin/Main.java
@@ -1,11 +1,8 @@
 package ru.mar4elkin;
 
-import ru.mar4elkin.models.CMDatabaseWrapper;
-
 public class Main {
 
     public static void main(String[] args) {
-        CMDatabaseWrapper dbw = new CMDatabaseWrapper("se", "");
-        System.out.print(dbw);
+        System.out.print("Hi!");
     }
 }
--- src/main/java/ru/mar4elkin/models/interfaces/ModelQuery.java
+++
@@ -1,7 +0,0 @@
-package ru.mar4elkin.models.interfaces;
-
-import java.util.List;
-
-public interface ModelQuery<T> {
-    public List<T> select(String queryText);
-}
