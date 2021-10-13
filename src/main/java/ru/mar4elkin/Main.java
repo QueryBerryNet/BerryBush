fb53fa1954b39ac9f35a6ff620b1eda17b618887
Mark <googlcraftlol@gmail.com>
2021-10-13 03:17:17 +0300

    new main for testing

--- src/main/java/ru/mar4elkin/Main.java
+++ src/main/java/ru/mar4elkin/Main.java
@@ -1,37 +1,27 @@
 package ru.mar4elkin;
-
-import ru.mar4elkin.datastorage.CBase;
 import ru.mar4elkin.datastorage.CDevice;
-import ru.mar4elkin.devices.CBaseDevice;
 import ru.mar4elkin.devices.CThermometer;
 import ru.mar4elkin.devices.CWindow;
 import ru.mar4elkin.devices.enums.EAvailableDevices;
-import ru.mar4elkin.devices.enums.EWindowPosition;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        CThermometer cThermometer = new CThermometer(
-                1, "127.0.0.1", "test_name", new Date(2021, 2, 3),
-                EAvailableDevices.Thermometer, 37f
-        );
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
 
-        CWindow cWindow = new CWindow(
-                1, "127.0.0.1", "test_name", new Date(2021, 2, 3),
-                EAvailableDevices.Window, EWindowPosition.Closed
-        );
+        CDevice cDevice = new CDevice("mysql", "127.0.0.1", "berry_brush", "root", "");
 
-        ArrayList<CBaseDevice> iotDeviceList = new ArrayList<CBaseDevice>();
-        iotDeviceList.add(cThermometer);
-        iotDeviceList.add(cWindow);
+        ArrayList<Class> iotDeviceList = new ArrayList<>();
+        iotDeviceList.add(CThermometer.class);
+        iotDeviceList.add(CWindow.class);
 
-        CDevice cDevice = new CDevice("mysql", "127.0.0.1", "laravel", "root", "", iotDeviceList);
         cDevice.createProjectDirectory();
-        cDevice.initializeDevice();
+        cDevice.initDatabase(iotDeviceList);
+
+        CThermometer temp = new CThermometer("127.0.0.1", "TestThermometer", null, EAvailableDevices.Thermometer, 37.6f);
+        cDevice.addDevice(temp);
 
 
     }
